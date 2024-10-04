816ee75295dfc3155c92894269d714b8b8ed0339
yuri-sousa-avenue <yuri.sousa@avenue.us>
2024-10-04 13:58:41 -0300

    concatenação

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+let numeroSecreto = 2;
+console.log(numeroSecreto)
 let chute =prompt('Escolha um número entre 1 e 30')
 
 if(numeroSecreto == chute ){
-    console.log('Isso ai!!, Você descobriu o número secreto(5)'); 
+    alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto}`); 
+}else{
+    alert('Você errou :( !!!')
 }

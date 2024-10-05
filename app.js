f1e9af35420c96e428319398f7ce055369b1b6e3
yuri-sousa-avenue <yuri.sousa@avenue.us>
2024-10-05 15:07:14 -0300

    operador ternario

--- app.js
+++ app.js
@@ -22,9 +22,13 @@ while(chute != numeroSecreto){
        tentativas++
     }
 }
-if(tentativas>1){
-    alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); 
-}else{
-    alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`); 
+let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`); 
 
-}
+
+// if(tentativas>1){
+//     alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); 
+// }else{
+//     alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`); 
+
+// }

3504e34d6b548a3346fd07a6fafe6a993266283a
yuri-sousa-avenue <yuri.sousa@avenue.us>
2024-10-05 14:56:51 -0300

    contador

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 2;
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute ;
 
+let tentativas = 1;
 //enquanto
 while(chute != numeroSecreto){
-    chute =prompt('Escolha um número entre 1 e 30')
-
+    chute =prompt('Escolha um número entre 1 e 30');
+    
     if(numeroSecreto == chute ){
-        alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto}`); 
+        alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`); 
     }else{
        if(chute>numeroSecreto) {
         alert(`O número secreto é maior que ${chute}`);
        }else{
         alert(`O número secreto é maior que o ${chute}`);
        }
+       //tentativas = tentativas + 1;
+       tentativas++
     }
 }

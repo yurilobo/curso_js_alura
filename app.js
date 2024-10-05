2b330e33a8d116a6e70c1c987da612b7273bda6e
yuri-sousa-avenue <yuri.sousa@avenue.us>
2024-10-05 14:38:47 -0300

    novas condicionais

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ let chute =prompt('Escolha um número entre 1 e 30')
 if(numeroSecreto == chute ){
     alert(`Isso ai!!, Você descobriu o número secreto ${numeroSecreto}`); 
 }else{
-    alert('Você errou :( !!!')
+   if(chute>numeroSecreto) {
+    alert(`O número secreto é maior que ${chute}`);
+   }else{
+    alert(`O número secreto é maior que o ${chute}`);
+   }
 }
